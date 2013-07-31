a610730965bec9e604c4813fdf2c59d34b4f9944
Tobias von Klipstein <klipstein@uxebu.com>
2013-07-31 23:22:18 +0200

    Fix downloads

--- install.js
+++ install.js
@@ -7,8 +7,8 @@ var url = require('url');
 
 var AdmZip = require('adm-zip');
 var kew = require('kew');
+var mkdirp = require('mkdirp');
 var npmconf = require('npmconf');
-var tmp = require('tmp');
 
 var webdriver = require('./lib/index.js');
 
@@ -19,49 +19,59 @@ npmconf.load(function(err, conf) {
     process.exit(1);
     return;
   }
-  
+
   if (!webdriver.platform) {
     console.log('Unexpected platform or architecture:', process.platform, process.arch);
     process.exit(1);
   }
 
-  var tmpPath;
+  var tmpPath = findSuitableTempDirectory(conf.get('tmp'));
+  var promise = kew.resolve(true);
 
-  var promise = getTmpDirectory()
-    .then(function(tmpDirectory) {
-      tmpPath = tmpDirectory;
+  if (!fs.existsSync(path.join(tmpPath, path.basename(webdriver.selenium.downloadUrl)))) {
+    promise = promise.then(function() {
       return downloadFile(
         conf.get('proxy'),
         webdriver.selenium.downloadUrl,
-        webdriver.selenium.path
+        path.join(tmpPath, path.basename(webdriver.selenium.downloadUrl))
       );
     })
-    .then(function() {
+  }
+  promise.then(function() {
+    fs.createReadStream(path.join(tmpPath, path.basename(webdriver.selenium.downloadUrl)))
+      .pipe(fs.createWriteStream(webdriver.selenium.path));
+  });
+
+  if (!fs.existsSync(path.join(tmpPath, path.basename(webdriver.chromedriver.downloadUrl)))) {
+    promise = promise.then(function() {
       return downloadFile(
         conf.get('proxy'),
         webdriver.chromedriver.downloadUrl,
         path.join(tmpPath, path.basename(webdriver.chromedriver.downloadUrl))
       );
-    })
-    .then(function() {
-      return extractDownload(
-        path.join(tmpPath, path.basename(webdriver.chromedriver.downloadUrl)),
-        path.dirname(webdriver.chromedriver.path)
-      );
-    })
-    .then(function() {
-      return fixFilePermissions(webdriver.chromedriver.path);
     });
+  }
+  promise.then(function() {
+    return extractDownload(
+      path.join(tmpPath, path.basename(webdriver.chromedriver.downloadUrl)),
+      path.dirname(webdriver.chromedriver.path)
+    );
+  })
+  .then(function() {
+    return fixFilePermissions(webdriver.chromedriver.path);
+  });
 
   if (webdriver.iedriver.downloadUrl) {
-    promise = promise.then(function() {
-      return downloadFile(
-        conf.get('proxy'),
-        webdriver.iedriver.downloadUrl,
-        path.join(tmpPath, path.basename(webdriver.iedriver.downloadUrl))
-      );
-    })
-    .then(function() {
+    if (!fs.existsSync(path.join(tmpPath, path.basename(webdriver.iedriver.downloadUrl)))) {
+      promise = promise.then(function() {
+        return downloadFile(
+          conf.get('proxy'),
+          webdriver.iedriver.downloadUrl,
+          path.join(tmpPath, path.basename(webdriver.iedriver.downloadUrl))
+        );
+      });
+    }
+    promise.then(function() {
       return extractDownload(
         path.join(tmpPath, path.basename(webdriver.iedriver.downloadUrl)),
         path.dirname(webdriver.iedriver.path)
@@ -71,7 +81,7 @@ npmconf.load(function(err, conf) {
       return fixFilePermissions(webdriver.iedriver.path);
     });
   }
-  
+
   promise.then(function() {
     console.log([
       'Done. Selenium, Chromedriver',
@@ -80,22 +90,37 @@ npmconf.load(function(err, conf) {
       path.dirname(webdriver.selenium.path)
     ].join(''));
   })
-  .fail(function (err) {
+  .fail(function(err) {
     console.error('Installation of Selenium, Chromedriver or IEDriver failed', err.stack);
-    process.exit(1)
+    process.exit(1);
   })
 });
 
-function getTmpDirectory() {
-  var deferred = kew.defer()
-  tmp.dir({mode: 0750, prefix: 'webdriver-setup_'}, function (error, path) {
-    if (error) {
-      deferred.reject('Error creating temporary directory: ' + error);
+function findSuitableTempDirectory(npmTmpDir) {
+  var now = Date.now();
+  var candidateTmpDirs = [
+    process.env.TMPDIR || '/tmp',
+    npmTmpDir,
+    path.join(process.cwd(), 'tmp')
+  ];
+
+  for (var i = 0; i < candidateTmpDirs.length; i++) {
+    var candidatePath = path.join(candidateTmpDirs[i], 'webdriver');
+
+    try {
+      mkdirp.sync(candidatePath, '0777');
+      var testFile = path.join(candidatePath, now + '.tmp');
+      fs.writeFileSync(testFile, 'test');
+      fs.unlinkSync(testFile);
+      return candidatePath;
+    } catch (e) {
+      console.log(candidatePath, 'is not writable:', e.message);
     }
-    deferred.resolve(path);
-  });
-  return deferred.promise;
-};
+  }
+
+  console.error('Cannot find a writable tmp directory');
+  process.exit(1);
+}
 
 function downloadFile(proxy, downloadUrl, downloadPath) {
   console.log('Downloading', downloadUrl);
@@ -105,80 +130,84 @@ function downloadFile(proxy, downloadUrl, downloadPath) {
 
 function getRequestOptions(proxyUrl, downloadUrl) {
   if (proxyUrl) {
-    var options = url.parse(proxyUrl)
-    options.path = downloadUrl
-    options.headers = { Host: url.parse(downloadUrl).host }
+    var options = url.parse(proxyUrl);
+    options.path = downloadUrl;
+    options.headers = { Host: url.parse(downloadUrl).host };
     // Turn basic authorization into proxy-authorization.
     if (options.auth) {
-      options.headers['Proxy-Authorization'] = 'Basic ' + new Buffer(options.auth).toString('base64')
-      delete options.auth
+      options.headers['Proxy-Authorization'] = 'Basic ' + new Buffer(options.auth).toString('base64');
+      delete options.auth;
     }
 
-    return options
+    return options;
   } else {
-    return url.parse(downloadUrl)
+    return url.parse(downloadUrl);
   }
 }
 
 function requestBinary(requestOptions, filePath) {
-  var deferred = kew.defer()
+  var deferred = kew.defer();
 
-  var count = 0
-  var notifiedCount = 0
-  var outFile = fs.openSync(filePath, 'w')
+  var count = 0;
+  var notifiedCount = 0;
+  var filePath = filePath;
+  var tmpFilePath = filePath + '.tmp';
+  var outFile = fs.openSync(tmpFilePath, 'w');
 
-  var client = http.get(requestOptions, function (response) {
-    var status = response.statusCode
-    console.log('Receiving...')
+  var client = http.get(requestOptions, function(response) {
+    var status = response.statusCode;
+    console.log('Receiving...');
 
     if (status === 200) {
-      response.addListener('data',   function (data) {
-        fs.writeSync(outFile, data, 0, data.length, null)
-        count += data.length
+      response.addListener('data', function(data) {
+        fs.writeSync(outFile, data, 0, data.length, null);
+        count += data.length;
         if ((count - notifiedCount) > 800000) {
-          console.log('Received ' + Math.floor(count / 1024) + 'K...')
-          notifiedCount = count
+          console.log('Received ' + Math.floor(count / 1024) + 'K...');
+          notifiedCount = count;
         }
       })
 
-      response.addListener('end',   function () {
-        console.log('Received ' + Math.floor(count / 1024) + 'K total.')
-        fs.closeSync(outFile)
-        deferred.resolve(true)
+      response.addListener('end', function() {
+        console.log('Received ' + Math.floor(count / 1024) + 'K total.');
+        fs.closeSync(outFile);
+        // just write the final file when download was completed
+        fs.createReadStream(tmpFilePath).pipe(fs.createWriteStream(filePath));
+        deferred.resolve(true);
       })
 
     } else {
-      client.abort()
-      deferred.reject('Error with http request: ' + util.inspect(response.headers))
+      client.abort();
+      deferred.reject(new Error('Error with http request: ' + util.inspect(response.headers)));
     }
-  })
+  });
 
-  return deferred.promise
+  return deferred.promise;
 }
 
 function extractDownload(filePath, extractToPath) {
-  var deferred = kew.defer()
-  var options = {cwd: extractToPath}
+  var deferred = kew.defer();
+  var options = {cwd: extractToPath};
 
-  console.log('Extracting zip contents')
+  console.log('Extracting zip contents');
   try {
-    var zip = new AdmZip(filePath)
-    zip.extractAllTo(extractToPath, true)
-    deferred.resolve(true)
+    var zip = new AdmZip(filePath);
+    zip.extractAllTo(extractToPath, true);
+    deferred.resolve(true);
   } catch (err) {
-    deferred.reject('Error extracting archive ' + err.stack)
+    deferred.reject(new Error('Error extracting archive ' + err.stack));
   }
-  return deferred.promise
+  return deferred.promise;
 }
 
 function fixFilePermissions(filePath) {
   // Check that the binary is user-executable and fix it if it isn't (problems with unzip library)
   if (process.platform != 'win32') {
-    var stat = fs.statSync(filePath)
+    var stat = fs.statSync(filePath);
     // 64 == 0100 (no octal literal in strict mode)
     if (!(stat.mode & 64)) {
-      console.log('Fixing file permissions')
-      fs.chmodSync(filePath, '755')
+      console.log('Fixing file permissions');
+      fs.chmodSync(filePath, '755');
     }
   }
 }
